eee5ad322fc7649b1f2f4902aa2c2969e9d013ff
Березин Дмитрий Борисович <laserino@macbook-pro-berezin.local>
2019-01-06 20:23:25 +0300

    Initial Commit

---
+++ lab_1_3_6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lab_1_3_6
+//
+//  Created by Березин Дмитрий Борисович on 06/01/2019.
+//  Copyright © 2019 Dmitry Berezin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
